508e01f24b23887c852d8c8a2692e4b14b1fde12
TheBesterCode <thebestergamergd@gmail.com>
2020-05-08 18:11:39 -0500

    Initial commit

---
+++ Serv/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.db.models import CASCADE
+
+
+class StudentsList(models.Model):
+    name = models.CharField(max_length=16, default='')
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    list = models.ForeignKey(
+        StudentsList,
+        on_delete=CASCADE,
+        default=None
+    )
+
+    def __str__(self):
+        return '{} {}'.format(self.last_name, self.first_name)
+
+
+class Teacher(models.Model):
+    key = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=20, default='')
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE,
+        default=None
+    )
+    students = models.OneToOneField(
+        StudentsList,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class GroupRegisters(models.Model):
+    name = models.CharField(max_length=24)
+    details = models.CharField(max_length=64)
+    date = models.DateField()
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        default=None
+    )
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE,
+        default=None
+    )
+
+    def __str__(self):
+        return '{}'.format(self.name)
---
+++ senv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/saul/Documentos/Kaerdos/reServ/senv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
